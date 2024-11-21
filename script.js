20f4d47f71f9b25574e8170d5be349d9b4054d4a
RangaPramod02 <rangapramodmekala@gmail.com>
2024-11-21 17:12:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("recipeButton").addEventListener("click", function() {
+    const nutritionalInfo = document.getElementById("nutritionalInfo");
+    nutritionalInfo.classList.toggle("hidden");
+});
